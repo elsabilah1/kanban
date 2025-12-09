fa685acf6a9056e6234d0510f404fd53974ef0fe
Elsa Sabilah <elsabilah1@gmail.com>
2025-12-09 23:32:31 +0700

    feat: updated dummy header & sidebar

--- resources/js/components/header.tsx
+++ resources/js/components/header.tsx
@@ -1,22 +1,35 @@
+import { Board } from '@/types';
+import { Dispatch, SetStateAction } from 'react';
 import iconChevronDown from '../../assets/icon-chevron-down.svg';
+import iconMoreOption from '../../assets/icon-vertical-ellipsis.svg';
 import logoMobile from '../../assets/logo-mobile.svg';
 import Button from './button';
 
-export default function Header() {
+interface HeaderProps {
+    boards: Board[];
+    selectedBoard: Board;
+    setSelectedBoard: Dispatch<SetStateAction<Board>>;
+}
+
+export default function Header({ boards, selectedBoard, setSelectedBoard }: HeaderProps) {
     return (
         <div className="flex h-16 items-center border-b border-lines-light bg-white md:h-20 dark:border-lines-dark dark:bg-dark-grey">
             <div className="flex flex-1 items-center gap-4 px-4 md:hidden">
                 <img src={logoMobile} alt="Logo mobile" />
                 <div className="flex items-center gap-2">
                     <h1 className="line-clamp-1 text-lg font-bold" title="Platform Launch">
-                        Platform Launch
+                        {selectedBoard.name}
                     </h1>
                     <img src={iconChevronDown} alt="Icon chevron down" />
                 </div>
             </div>
-            <h1 className="ml-6 hidden flex-1 text-xl font-bold md:block">Platform Launch</h1>
-            <Button></Button>
-            <Button></Button>
+            <h1 className="ml-6 hidden flex-1 text-xl font-bold md:block">{selectedBoard.name}</h1>
+            <Button>
+                <span className="capitalize">+ add new task</span>
+            </Button>
+            <button className="mr-2 cursor-pointer px-6">
+                <img src={iconMoreOption} alt="Icon more option" />
+            </button>
         </div>
     );
 }
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,21 +1,31 @@
 import Button from '@/components/button';
 import Header from '@/components/header';
 import Sidebar from '@/components/sidebar';
+import { Board } from '@/types';
 import { Head } from '@inertiajs/react';
+import { useState } from 'react';
+import dummyData from '../../assets/data.json';
 
 export default function Dashboard() {
+    const boards = dummyData.boards;
+    const [selectedBoard, setSelectedBoard] = useState<Board>(boards[0]);
+
     return (
         <>
             <Head title="Dashboard" />
             <div className="flex min-h-screen bg-light-grey text-black dark:bg-very-dark-grey dark:text-white">
-                <Sidebar />
+                <Sidebar boards={boards} selectedBoard={selectedBoard} setSelectedBoard={setSelectedBoard} />
                 <div className="flex flex-1 flex-col">
-                    <Header />
+                    <Header boards={boards} selectedBoard={selectedBoard} setSelectedBoard={setSelectedBoard} />
                     <main className="grid flex-1 place-items-center p-6">
-                        <div className="text-center text-pretty md:min-w-md md:px-18">
-                            <p className="mb-6 text-lg font-bold text-medium-grey">This board is empty. Create a new column to get started.</p>
-                            <Button></Button>
-                        </div>
+                        {selectedBoard.columns.length === 0 && (
+                            <div className="text-center text-pretty md:min-w-md md:px-18">
+                                <p className="mb-6 text-lg font-bold text-medium-grey">This board is empty. Create a new column to get started.</p>
+                                <Button>
+                                    <span className="capitalize">+ add new column</span>
+                                </Button>
+                            </div>
+                        )}
                     </main>
                 </div>
             </div>

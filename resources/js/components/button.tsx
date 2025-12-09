a8f2282079ae5144e37ad05956ec5fda32ac360c
Elsa Sabilah <elsabilah1@gmail.com>
2025-12-09 23:31:24 +0700

    feat: updated button component

--- resources/js/components/button.tsx
+++ resources/js/components/button.tsx
@@ -1,7 +1,15 @@
-export default function Button() {
+import { cn } from '@/lib/utils';
+
+export default function Button({ className, children, ...props }: React.ComponentProps<'button'>) {
     return (
-        <button>
-            <span>btn</span>
+        <button
+            className={cn(
+                'h-12 cursor-pointer rounded-full bg-main-purple px-6 text-sm font-bold text-white transition hover:bg-main-purple-hover',
+                className,
+            )}
+            {...props}
+        >
+            {children}
         </button>
     );
 }
